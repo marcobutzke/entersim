07f1d1b582289e6db389fbb62a8bc450e1c49fd6
Marco Aurélio Butzke <marcobutzke@gmail.com>
2023-12-08 15:53:49 -0300

    segundo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout='wide')
 @st.cache_data
 def load_database():
   return pd.read_parquet('brasil_estados.parquet'), pd.read_parquet('SSEuropa.parquet')
