e636a8aac29a8a0583e3d2dd1f07ee1a9c9756d9
marcobutzke <58192256+marcobutzke@users.noreply.github.com>
2023-12-08 15:01:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import pandas as pd
 
-st.write('Hello!!!')
+@st.cache_data
+def load_database():
+  return pd.read_excel('brasil_estados.xlsx'), pd.read_parquet('SSEuropa.parquet)
 
-if st.button('Click me!!!'):
-  st.balloons()
+estados, europa = load_database()
+
+st.dataframe(estados)
+
+st.dataframe(europa)                                                             
