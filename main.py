286c7f335cd05c1fa82dd0de0a0e2912ccf5cff6
marcobutzke <58192256+marcobutzke@users.noreply.github.com>
2023-12-08 15:09:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 @st.cache_data
 def load_database():
-  return pd.read_excel('brasil_estados.xlsx'), pd.read_parquet('SSEuropa.parquet')
+  return pd.read_excel('brasil_estados.parquet'), pd.read_parquet('SSEuropa.parquet')
 
 estados, europa = load_database()
 
